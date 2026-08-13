6c88fcc12caee4549624400afffe194aa0608fbf
kiyonya <481366085@qq.com>
2026-08-13 15:43:37 +0800

    chore: 移除未使用的第三方库,创建编译脚本

---
+++ compile.ps1
@@ -0,0 +1,3 @@
+dart compile exe main.dart --output=./build/cloudflare-speed-fix-win_x64.exe --target-os=windows --target-arch=x64
+
+dart compile exe main.dart --output=./build/cloudflare-speed-fix-win_x86.exe --target-os=windows --target-arch=ia32
