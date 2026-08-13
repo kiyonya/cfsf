cbba3aec3bddea43c841c7a8c110694f42fde504
kiyonya <481366085@qq.com>
2026-08-13 23:46:05 +0800

    build: 更新编译命令

--- compile.ps1
+++ compile.ps1
@@ -1,3 +1,2 @@
-dart compile exe main.dart --output=./build/cloudflare-speed-fix-win_x64.exe --target-os=windows --target-arch=x64
-
-dart compile exe main.dart --output=./build/cloudflare-speed-fix-win_x86.exe --target-os=windows --target-arch=ia32
+dart compile exe main.dart --output=./build/cfsf.exe --target-os=windows --target-arch=x64
+dart compile exe probetool.dart --output=./build/probetool.exe --target-os=windows --target-arch=x64
